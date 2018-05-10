f153e632f6b6468841bf1f4d2f10b4b8a136626b
e <410635270@qq.com>
2018-05-10 14:44:14 +0800

    commit

--- mywechatbot-V-1.0.py
+++ mywechatbot-V-1.0.py
@@ -118,7 +118,7 @@ def group_reply_media(msg):
 
  # 扫二维码登录
 # itchat.auto_login(hotReload=True, loginCallback=lc, exitCallback=ec)
-itchat.auto_login()
+itchat.auto_login(hotReload=True, loginCallback=lc, exitCallback=ec)
 
 # 音乐控制
 # itchat.send(HELP_MSG, 'filehelper') 
